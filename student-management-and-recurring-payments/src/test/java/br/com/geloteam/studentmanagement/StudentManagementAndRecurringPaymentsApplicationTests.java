699df49365911b0991944bdc388c13d352c7b472
viitorgonzalez <pastor123.vs@gmail.com>
2026-02-13 13:09:56 -0300

    First Commit

---
+++ student-management-and-recurring-payments/src/test/java/br/com/geloteam/studentmanagement/StudentManagementAndRecurringPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geloteam.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementAndRecurringPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
