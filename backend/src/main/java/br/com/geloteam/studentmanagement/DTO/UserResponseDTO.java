142278ffb0b0b8a75cbb35dbe714c4282ef71f91
viitorgonzalez <pastor123.vs@gmail.com>
2026-03-17 14:54:50 -0300

    feat: created UserRepository

---
+++ backend/src/main/java/br/com/geloteam/studentmanagement/DTO/UserResponseDTO.java
@@ -0,0 +1,4 @@
+package br.com.geloteam.studentmanagement.DTO;
+
+public record UserDTO() {
+}
---
+++ backend/src/main/java/br/com/geloteam/studentmanagement/Repositories/CompanyRepository.java
@@ -0,0 +1,4 @@
+package br.com.geloteam.studentmanagement.Repositories;
+
+public interface CompanyRepository {
+}
