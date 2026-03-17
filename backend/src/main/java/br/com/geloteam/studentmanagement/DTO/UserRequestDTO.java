470995f1ee88a98cadb5fa969cceed315e25ffd2
viitorgonzalez <pastor123.vs@gmail.com>
2026-03-17 14:56:14 -0300

    feat: created UserRequestDTO

--- backend/src/main/java/br/com/geloteam/studentmanagement/DTO/UserRequestDTO.java
+++ backend/src/main/java/br/com/geloteam/studentmanagement/DTO/UserRequestDTO.java
@@ -1,4 +1,15 @@
 package br.com.geloteam.studentmanagement.DTO;
 
-public record UserRequestDTO() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UserRequestDTO(
+        @NotBlank String name,
+        @NotBlank @Email String email,
+        @NotBlank String password,
+        String cellphoneNumber,
+        @NotNull Long companyId
+) {
+    // to-do
 }
