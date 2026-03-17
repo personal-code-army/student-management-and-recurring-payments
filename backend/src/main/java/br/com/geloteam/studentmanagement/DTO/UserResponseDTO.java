6675ee715375ff8a643d2d8a1354b907e07cc585
viitorgonzalez <pastor123.vs@gmail.com>
2026-03-17 14:55:55 -0300

    feat: created UserResponseDTO

--- backend/src/main/java/br/com/geloteam/studentmanagement/DTO/UserResponseDTO.java
+++ backend/src/main/java/br/com/geloteam/studentmanagement/DTO/UserResponseDTO.java
@@ -1,4 +1,14 @@
 package br.com.geloteam.studentmanagement.DTO;
 
-public record UserDTO() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UserResponseDTO(
+        @NotBlank String name,
+        @NotBlank @Email String email,
+        String cellphoneNumber,
+        @NotNull Long companyId
+) {
+    // to-do
 }
