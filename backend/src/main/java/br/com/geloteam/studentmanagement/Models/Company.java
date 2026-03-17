791d5c53993b8ea3e436c8e1b79ff5ff3e847682
viitorgonzalez <pastor123.vs@gmail.com>
2026-03-17 12:06:21 -0300

    feat: created Company model

--- backend/src/main/java/br/com/geloteam/studentmanagement/Models/Company.java
+++ backend/src/main/java/br/com/geloteam/studentmanagement/Models/Company.java
@@ -1,4 +1,36 @@
 package br.com.geloteam.studentmanagement.Models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "companies")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String cnpj;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(name = "cellphone_number", nullable = false)
+    private String cellphoneNumber;
 }
