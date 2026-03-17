8783a1fd67614557c197441a64ce6ba6e45f8c86
viitorgonzalez <pastor123.vs@gmail.com>
2026-03-17 12:05:51 -0300

    feat: created Student model

--- backend/src/main/java/br/com/geloteam/studentmanagement/Models/Student.java
+++ backend/src/main/java/br/com/geloteam/studentmanagement/Models/Student.java
@@ -1,4 +1,27 @@
 package br.com.geloteam.studentmanagement.Models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plan_id")
+    private Plan plan;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private boolean active;
 }
