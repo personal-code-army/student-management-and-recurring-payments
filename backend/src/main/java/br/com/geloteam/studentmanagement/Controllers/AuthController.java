cb0c4cbed3ea5f47e840ca678bbca2ec988d12fa
viitorgonzalez <pastor123.vs@gmail.com>
2026-03-18 16:58:26 -0300

    feat: auth controller

--- backend/src/main/java/br/com/geloteam/studentmanagement/Controllers/AuthController.java
+++ backend/src/main/java/br/com/geloteam/studentmanagement/Controllers/AuthController.java
@@ -1,4 +1,36 @@
 package br.com.geloteam.studentmanagement.Controllers;
 
+import br.com.geloteam.studentmanagement.DTO.LoginRequestDTO;
+import br.com.geloteam.studentmanagement.DTO.LoginResponseDTO;
+import br.com.geloteam.studentmanagement.DTO.UserResponseDTO;
+import br.com.geloteam.studentmanagement.Services.AuthService;
+import br.com.geloteam.studentmanagement.Services.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO data) {
+        var response = authService.login(data);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDTO> me(Authentication authentication) {
+        return ResponseEntity.ok(authService.me(authentication));
+    }
 }
